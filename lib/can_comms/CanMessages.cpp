34bc99c6798b5464b06c8864b806441b3c6356c2
Kevin Gilliam <keving@arizona.edu>
2022-09-08 08:48:33 -0700

    simplified can message scheme

--- lib/can_comms/CanMessages.cpp
+++ lib/can_comms/CanMessages.cpp
@@ -1,13 +1,13 @@
- /// 
- ///  @ Author: Kevin Gilliam
- ///  @ Create Time: 2022-09-07 15:54:35
- ///  @ Modified by: Kevin Gilliam
- ///  @ Modified time: 2022-09-08 08:16:42
- ///  @ Description:
- ///
+///
+///  @ Author: Kevin Gilliam
+///  @ Create Time: 2022-09-07 15:54:35
+///  @ Modified by: Kevin Gilliam
+///  @ Modified time: 2022-09-08 08:25:34
+///  @ Description:
+///
 
 #include "CanMessages.h"
-#include <vector>
+#include <array>
 #include <tuple>
 #include <iostream>
 #include <algorithm>
@@ -15,26 +15,31 @@
 #include <FlexCAN_T4.h>
 #include <device.h>
 
+#define MAX_CAN_MESSAGES 0x4F // can be increased if needed
+
 void processMessage(uint32_t msgId);
 void invalidMessageHandler(char *guts, int len);
 
-typedef void (*msgHandler)(char *, int);
-typedef struct
+
+typedef std::array<MsgHandler, MAX_CAN_MESSAGES> MessageList;
+
+static MessageList messageHandlerList;
+
+void registerCanMessageHandler(uint16_t canId, MsgHandler fn)
 {
-    uint16_t msgId;
-    msgHandler msgCb;
-} messageListItem;
-
-typedef std::vector<messageListItem> MessageList;
-
-static MessageList messageList
-    {
-        {CAN_ID(INVALID_ID, INVALID_ID), &invalidMessageHandler}
-    };
+    messageHandlerList[canId] = fn;
+}
 
 void initializeCanMessages()
 {
+    for (int16_t ii = 0; ii < MAX_CAN_MESSAGES; ii++)
+    {
+        registerCanMessageHandler(ii, defaultMessageHandler);
+    }
+    registerCanMessageHandler(INVALID_ID, invalidMessageHandler);
 }
+
+
 
 void defaultMessageHandler(char *guts, int len)
 {
@@ -49,9 +54,9 @@ void invalidMessageHandler(char *guts, int len)
     std::string msgGuts(guts, len);
     std::stringstream ss;
     ss << "Invalid Message ID. Payload: ";
-    for(int ii = 0; ii < 8; ii++)
-    { 
-        ss << std::hex << (uint8_t)guts[ii]; 
+    for (int ii = 0; ii < 8; ii++)
+    {
+        ss << std::hex << (uint8_t)guts[ii];
     }
     ss << std::endl;
     TEST_SERIAL.println(ss.str().c_str());
@@ -60,19 +65,10 @@ void invalidMessageHandler(char *guts, int len)
 void processMessage(const CAN_message_t &msg)
 {
     uint32_t id = msg.id;
-    std::stringstream ss;
-    ss << "Processing message ID: " << id << std::endl;
-    const auto itr{
-        std::find_if(
-            messageList.begin(),
-            messageList.end(),
-            [id](const auto &msgListItem)
-            {
-                return (msgListItem.msgId == id);
-            })};
-
-    if (itr != messageList.end())
-        (*itr).msgCb((char *)msg.buf, msg.len);
-    else
-        defaultMessageHandler((char *)msg.buf, msg.len);
+    MsgHandler handlerFn = defaultMessageHandler;
+    if(id <= MAX_CAN_MESSAGES)
+    {
+        handlerFn = messageHandlerList[id];
+    }
+    handlerFn((char *)msg.buf, msg.len);
 }
--- lib/can_comms/CanMessages.h
+++ lib/can_comms/CanMessages.h
@@ -4,15 +4,13 @@
 #include <FlexCAN_T4.h>
 
 ///////////////// SENDER ID'S /////////////////
-#define INVALID_ID          0x0
+#define INVALID_ID          0x00
+#define CTRL_PC             0x10
+#define PFC_CTRL            0x20
+#define PEDESTAL_CTRL       0x30
+#define TEC_CTRL            0x40
 
-#define CTRL_PC             0xA
-#define PFC_CTRL            0xB
-#define PEDESTAL_CTRL       0xC
-#define TEC_CTRL            0xD
-
-#define CAN_SNDR_ID_MASK    0x0F00
-#define CAN_SNDR_ID_SHFT    0x4
+#define CAN_SNDR_ID_MASK    0xF0
 
 ///////////////// CTRL PC MESSAGE ID's /////////////////
 #define REQEST_STATS        0x01
@@ -24,15 +22,20 @@
 #define CMD_DEC_POLY_B      0x07
 #define CMD_DEC_POLY_C      0x08
 #define AZ_POSN_REQ         0x09
-#define EL_POSN_REQ         0x10
+#define EL_POSN_REQ         0x0A
 
-#define CAN_MSG_ID_MASK     0x00FF
-#define CAN_MSG_ID_SHFT     0x0
+#define CAN_MSG_ID_MASK     0x0F
 
 
 ///////////////// MACROS /////////////////
-#define CAN_ID(s,n) (uint32_t) (((s&CAN_SNDR_ID_MASK)<<CAN_SNDR_ID_SHFT) | ((s&CAN_MSG_ID_MASK)<<CAN_MSG_ID_SHFT))
+// #define CAN_ID(s,n) (uint32_t) (((s&CAN_SNDR_ID_MASK)<<CAN_SNDR_ID_SHFT) | ((s&CAN_MSG_ID_MASK)<<CAN_MSG_ID_SHFT))
+
+///////////////// TYPES /////////////////
+typedef void (*MsgHandler)(char *, int);
 
 
+
+///////////////// FUNCTIONS /////////////////
 void initializeCanMessages();
 void processMessage(const CAN_message_t &msg);
+void registerCanMessageHandler(uint16_t canId, MsgHandler fn);
