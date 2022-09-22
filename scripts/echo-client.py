9988dba8522f466c98c289f7f77a13a83e5db2a2
Kevin Gilliam <keving@arizona.edu>
2022-09-22 12:15:21 -0700

    added some python test scripts

--- scripts/echo-client.py
+++ scripts/echo-client.py
@@ -2,8 +2,11 @@
 
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+# HOST = "127.0.0.1"  # The server's hostname or IP address
+# PORT = 65432  # The port used by the server
+# HOST = "192.168.1.177"
+HOST = "192.168.190.101"
+PORT = 4400  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
--- scripts/echo-server.py
+++ scripts/echo-server.py
@@ -2,17 +2,22 @@
 
 import socket
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+# HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+# PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
+HOST = "192.168.190.101"
+PORT = 4400  # The port used by the server
+
+print("Attempting to connect.")
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.bind((HOST, PORT))
+    server.listen()
+    client_socket, client_address = server.accept()
+    with client_socket:
+        print(f"Connected by {client_address}")
         while True:
-            data = conn.recv(1024)
+            data = client_socket.recv(1024)
             if not data:
                 break
-            conn.sendall(data)
+            client_socket.sendall(data)
