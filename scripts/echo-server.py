aea12e5e0d54aa5955218f7d920353f76a1e0408
Kevin Gilliam <keving@arizona.edu>
2022-09-29 18:11:52 -0700

    added changing ra/dec vals

--- scripts/echo-server.py
+++ scripts/echo-server.py
@@ -5,7 +5,8 @@ from xml.dom.minidom import TypeInfo
 
 # HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 # PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
-
+raVal = 0.0
+deVal = 0.0
 # HOST = "192.168.190.101"
 HOST = "localhost"
 # HOST = "192.168.121.177"
@@ -52,11 +53,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
                     # client_socket.sendall(b'abcd')
                 case 2:
                     print("Sending RA/DEC")
+                    raVal = raVal + 0.1
+                    deVal = deVal + 0.05
+                    raDecStr = "2#" + str(raVal) + ";" + str(deVal) + "^"
                     # client_socket.sendall(bytes("2#1.234").encode('utf-8'))
-                    client_socket.sendall("2#1.234;2.345^".encode('utf-8'))
+                    client_socket.sendall(raDecStr.encode('utf-8'))
                 case 3:
                     print("Sending Tracking Status")
-                    client_socket.sendall("3#3.456^".encode('utf-8'))
+                    client_socket.sendall("3#0.0^".encode('utf-8'))
                 case _:
                     toPrint = "(Default) Sending " + data.decode("utf-8")
                     print(toPrint)
